06539f5072467d2736afca4c3635f76885f9c05b
Elie-rgb <anakweellie@gmail.com>
2026-04-11 14:16:34 +0100

    init
    game over and main menu

--- survivors_game.gd
+++ survivors_game.gd
@@ -9,3 +9,13 @@ func spawn_mob():
 
 func _on_timer_timeout() -> void:
 	spawn_mob()
+
+
+func _on_player_health_depleted() -> void:
+	%Gameover.visible = true
+	get_tree().paused = true
+	
+	await get_tree().create_timer(3.0, true).timeout
+	
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main_menu.tscn")
